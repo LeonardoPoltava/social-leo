cbb8120d38f51b250f4d339728b738969cff182e
vladyslav-paradigma <leonardopoltava@gmail.com>
2020-02-03 10:18:44 +0200

    add build

---
+++ precache-manifest.22983cd167a79dbe0b585096dff4f300.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c9ec8c80d9e66d5f0ea84277e3f38b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8547096a6939f6c1a127",
+    "url": "/static/css/4.5fc5eaf6.chunk.css"
+  },
+  {
+    "revision": "34a3cc67d6504753e645",
+    "url": "/static/css/5.f3501cfa.chunk.css"
+  },
+  {
+    "revision": "34f1ffa4b4e57c769fcb",
+    "url": "/static/css/6.96f0c831.chunk.css"
+  },
+  {
+    "revision": "b9431ade401bb64bd9e9",
+    "url": "/static/css/7.11f47c6d.chunk.css"
+  },
+  {
+    "revision": "2f04653c91d1499657ab",
+    "url": "/static/css/8.e051d6a6.chunk.css"
+  },
+  {
+    "revision": "b7033a64b1c7f7ef9c9d",
+    "url": "/static/css/main.77169167.chunk.css"
+  },
+  {
+    "revision": "f1df70416fd9e20f2bfe",
+    "url": "/static/js/0.10d3b76b.chunk.js"
+  },
+  {
+    "revision": "599494d77f3c80e85b47",
+    "url": "/static/js/3.f725de87.chunk.js"
+  },
+  {
+    "revision": "8547096a6939f6c1a127",
+    "url": "/static/js/4.bad6de50.chunk.js"
+  },
+  {
+    "revision": "34a3cc67d6504753e645",
+    "url": "/static/js/5.d3e277f0.chunk.js"
+  },
+  {
+    "revision": "34f1ffa4b4e57c769fcb",
+    "url": "/static/js/6.7f1ec835.chunk.js"
+  },
+  {
+    "revision": "b9431ade401bb64bd9e9",
+    "url": "/static/js/7.84d2c3b7.chunk.js"
+  },
+  {
+    "revision": "2f04653c91d1499657ab",
+    "url": "/static/js/8.aa432bb8.chunk.js"
+  },
+  {
+    "revision": "b7033a64b1c7f7ef9c9d",
+    "url": "/static/js/main.81d240cf.chunk.js"
+  },
+  {
+    "revision": "b77d2b261d9d646e818a",
+    "url": "/static/js/runtime-main.5e263b62.js"
+  },
+  {
+    "revision": "a93f6632526b47237534675e1174cc61",
+    "url": "/static/media/logo.a93f6632.png"
+  }
+]);
