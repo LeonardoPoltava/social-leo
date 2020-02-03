2e73e1ea096fdc89d615e36bfdc3ab036c6d8bf2
vladyslav-paradigma <leonardopoltava@gmail.com>
2020-02-03 10:58:52 +0200

    test-commit

---
+++ build/precache-manifest.7432419ad67cfb8e6249b2ee67389db1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacfa9aa4b28f122743b606a99776440",
+    "url": "/social-leo/index.html"
+  },
+  {
+    "revision": "2473bc20a84348ec738e",
+    "url": "/social-leo/static/css/4.5fc5eaf6.chunk.css"
+  },
+  {
+    "revision": "035b93fb748a248ba487",
+    "url": "/social-leo/static/css/5.f3501cfa.chunk.css"
+  },
+  {
+    "revision": "13aa33c2f030d39e7efc",
+    "url": "/social-leo/static/css/6.96f0c831.chunk.css"
+  },
+  {
+    "revision": "d96901b21ea77266b2a4",
+    "url": "/social-leo/static/css/7.11f47c6d.chunk.css"
+  },
+  {
+    "revision": "e979b80ee3dd93c8a9f1",
+    "url": "/social-leo/static/css/8.e051d6a6.chunk.css"
+  },
+  {
+    "revision": "6a8a7070aa5c990d3213",
+    "url": "/social-leo/static/css/main.77169167.chunk.css"
+  },
+  {
+    "revision": "f1df70416fd9e20f2bfe",
+    "url": "/social-leo/static/js/0.10d3b76b.chunk.js"
+  },
+  {
+    "revision": "599494d77f3c80e85b47",
+    "url": "/social-leo/static/js/3.f725de87.chunk.js"
+  },
+  {
+    "revision": "2473bc20a84348ec738e",
+    "url": "/social-leo/static/js/4.1cb572a9.chunk.js"
+  },
+  {
+    "revision": "035b93fb748a248ba487",
+    "url": "/social-leo/static/js/5.f7b46f8f.chunk.js"
+  },
+  {
+    "revision": "13aa33c2f030d39e7efc",
+    "url": "/social-leo/static/js/6.673b88f4.chunk.js"
+  },
+  {
+    "revision": "d96901b21ea77266b2a4",
+    "url": "/social-leo/static/js/7.1f9d413e.chunk.js"
+  },
+  {
+    "revision": "e979b80ee3dd93c8a9f1",
+    "url": "/social-leo/static/js/8.94f6c53c.chunk.js"
+  },
+  {
+    "revision": "6a8a7070aa5c990d3213",
+    "url": "/social-leo/static/js/main.2f3557d3.chunk.js"
+  },
+  {
+    "revision": "d264462ce0d5cdac9c71",
+    "url": "/social-leo/static/js/runtime-main.45ae95cb.js"
+  },
+  {
+    "revision": "a93f6632526b47237534675e1174cc61",
+    "url": "/social-leo/static/media/logo.a93f6632.png"
+  }
+]);
